640df7fd1cf42e59cf938fc5c10c9f9c5391241f
CallCab8109 <43888656+CallCab8109@users.noreply.github.com>
2018-10-19 11:51:26 -0600

    added alive

--- js/main.js
+++ js/main.js
@@ -11,5 +11,14 @@ function getstate() {
     console.log(table)
    var rows = table.getElementsByTagName("tr");
     console.log(rows)
-    
+    var state = [];
+    for (var y=0; y<rows.length; y += 1) {
+        var row = rows [y];
+        var cells = row.getElementsByTagName("td");
+        for (var x=0; x<cells.length; x += 1) {
+            var cell = cells [x];
+            var alive = cell.classList.contains('alive');
+            console.log(alive)
+        }
+    }
 }
