034788bc28b9c24f50b4a8aaab94ba54b7e5c8ed
CallCab8109 <43888656+CallCab8109@users.noreply.github.com>
2018-11-02 11:16:13 -0600

    Update main.js
    eskitit

--- js/main.js
+++ js/main.js
@@ -15,13 +15,18 @@ function getstate() {
     for (var y=0; y<rows.length; y += 1) {
         var row = rows [y];
         var cells = row.getElementsByTagName("td");
+        
+        var rowValues = [];
+        
         for (var x=0; x<cells.length; x += 1) {
             var cell = cells [x];
             var alive = cell.classList.contains('alive');
             console.log(alive)
             var rowAlive = alive.getElementsByTagName 
             console.log(rowAlive)
+            rowvalues.append(alive)
         }
     }
+    state.append(rowValues);
 }
 // variable representing a row make row represent ray
